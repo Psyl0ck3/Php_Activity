3cfa3deab7adbc1b991df46159d84b273b4d2d4d
Glyzza Lei <92197857+Psyl0ck3@users.noreply.github.com>
2024-10-04 21:46:40 +0800

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="login.php" method="GET">
+    <form action="" method="POST">
         <p>
             <label for="Username">Username</label>
             <input type="text" name="user">
@@ -17,25 +17,67 @@
             <input type="password" name="pass">
         </p>
 
-        <input type= "submit" name="btn_login" value="Login">
+        <input type="submit" name="btn_login" value="Login">
         <br>
         <br>
-        <input type= "submit" name="btn_logout" value="Log Out">
+        <input type="submit" name="btn_logout" value="Log Out">
     </form>
     <?php
-        if (isset($_GET['btn_login'])) {
-            $usern = $_GET['user'];
-            $passw = $_GET['pass'];
+        session_start();
 
+        function login($user, $pass) {
+            if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
+                echo 'You are already logged in as ' . $_SESSION['user'] . '. Please log out first.';
+                return false;
+            } else {
+                $_SESSION['logged_in'] = true;
+                $_SESSION['user'] = $user;
+                $_SESSION['pass'] = $pass;
+                return true;
+            }
         }
 
-        //displaying username and password
-        echo 'User logged in: ' .$usern. '';
-        echo 'Password: ' .$passw. '';
-        
+        function logout() {
+            session_unset();
+            session_destroy();
+        }
 
+        function generateRandomPassword() {
+            $length = 10;
+            $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+{}[]:;<>?,./';
+            $randomPassword = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomPassword .= $characters[rand(0, strlen($characters) - 1)];
+            }
+            $hashedPassword = password_hash($randomPassword, PASSWORD_DEFAULT);
+            return $hashedPassword;
+        }
         
+        if (isset($_POST['btn_login'])) {
+            $usern = $_POST['user'];
+            $passw = $_POST['pass'];
+            if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
+                echo $_SESSION['user'] . ' is already logged in. Wait for the user to log out first.';
+            } else {
+                if (!empty($passw)) {
+                    $randomPassword = generateRandomPassword();
+                    if (login($usern, $randomPassword)) {
+                        echo 'User logged in: ' . $_SESSION['user'] . '';
+                        echo 'Password: ' . $_SESSION['pass'] . '';
+                    }
+                } else {
+                    if (login($usern, $passw)) {
+                        echo 'User logged in: ' . $_SESSION['user'] . '';
+                        echo 'Password: ' . $_SESSION['pass'] . '';
+                    }
+                }
+            }
+        }
 
+        if (isset($_POST['btn_logout'])) {
+            logout();
+            echo 'You have been logged out.';
+        }
     ?>
 </body>
 </html>
